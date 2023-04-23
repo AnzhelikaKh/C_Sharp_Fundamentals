07d7e05cc1cdbdab78be8ae4445740b03ea916fa
Anzhelika <Llike9750@gmail.com>
2023-04-23 04:08:07 +0300

    Homewor

---
+++ 01_Base/Homework.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Base_01
+{
+    internal class Homework
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TASK 1:");
+            Console.WriteLine("Enter the value of the side of the square: ");
+            int a = int.Parse(Console.ReadLine());
+            int area = a * a;
+            int perimeter = 4 * a;
+            Console.WriteLine($"Area of a square: {area}\nPerimeter of a square: {perimeter}");
+
+            //***********************
+
+            Console.WriteLine("TASK 2:");
+            string name;
+            int age;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine($"How old are you, {name}?");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your name is {name} and you are {age} years old ");
+
+            //***********************
+
+            Console.WriteLine("TASK 3:");
+            Console.WriteLine("Enter radius of a circle: ");
+            double r = double.Parse(Console.ReadLine());
+            double length = 2 * Math.PI * r;
+            double area2 = Math.PI * r * r;
+            double volume = 4.0 / 3.0 * Math.PI * Math.Pow(r, 3);
+            Console.WriteLine($"Length: {length}\nArea: {area2}\nVolume: {volume}");
+            
+            
+
+        }
+    }
+}
---
+++ 01_Base/Task.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+namespace Base_01 { 
+
+class Task
+{
+    static void main(string[] args)
+    {
+            Console.WriteLine("Task 1:");
+            int a, b;
+            Console.WriteLine("Enter number a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number b: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a - b = " + (a - b));
+            Console.WriteLine("a * b = " + (a * b));
+            if (b > 0)
+            {
+                int div = a / b;
+                Console.WriteLine("a / b =" + div);
+            }
+            else
+                Console.WriteLine("No,I can't do this!!!(Don't divide by 0)");
+            //Thread.Sleep(5000);
+            //Console.Clear();
+
+            Console.WriteLine("Task 2:");
+
+            Console.WriteLine("How are you?");
+            string answer = Console.ReadLine();
+            Console.WriteLine("You are " + answer);
+
+            Console.WriteLine("Task 3:");
+
+            Console.WriteLine("Enter three type char values: ");
+            Console.WriteLine("First one: ");
+            char numChar1 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second one: ");
+            char numChar2 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Third one: ");
+            char numChar3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You enter ({numChar1}), ({numChar2}), ({numChar3})");
+
+            Console.WriteLine("Task 4:");
+            
+            Console.WriteLine("Enter two int numbers: ");
+
+            Console.WriteLine("Enter first number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > 0 && number2 > 0)
+            {
+                Console.WriteLine("They are both positive");
+            }
+            else
+            {
+                Console.WriteLine("One fron number or both are negativ");
+            }
+        }
+    }
+
+}
